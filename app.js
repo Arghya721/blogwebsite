f8a26889583f77cdaed0a24a473986d9ff9f83aa
Arghya721 <60299552+Arghya721@users.noreply.github.com>
2021-10-25 22:46:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ passport.serializeUser(function(user, done) {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://blogmen.herokuapp.com/auth/google/callback",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
